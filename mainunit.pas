9270cd647296bc07e582b541a8d871122481ac1a
Nathan Campos <nathan@innoveworkshop.com>
2025-04-27 12:39:39 +0100

    Finished the layout of the application

--- mainunit.pas
+++ mainunit.pas
@@ -5,10 +5,30 @@ unit MainUnit;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls,
+  StdCtrls, Buttons;
 
 type
+
+  { TMainForm }
+
   TMainForm = class(TForm)
+    AddressBox: TComboBox;
+    BrowserList: TListView;
+    ToolbarImages: TImageList;
+    MainMenu: TMainMenu;
+    FileMenu: TMenuItem;
+    FileSaveMenuItem: TMenuItem;
+    FileExitMenu: TMenuItem;
+    Separator1: TMenuItem;
+    StatusBar: TStatusBar;
+    ToolBar: TToolBar;
+    BackButton: TToolButton;
+    ForwardButton: TToolButton;
+    RefreshButton: TToolButton;
+    GoButton: TToolButton;
+    ToolButtonDivider1: TToolButton;
+    UpButton: TToolButton;
   private
 
   public
